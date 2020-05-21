b5ba4784d534e4def76064dd61d4b72a5dcbac8e
Sivadej K <bombyk@gmail.com>
2020-05-20 22:12:38 -0800

    container component

--- src/TestLayout.js
+++ src/TestLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import TestCard from './TestCard';
-import { Grid } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import Header from './Header';
 import { useSelector } from 'react-redux';
 import ProductCard from './ProductCard';
@@ -12,23 +11,26 @@ const TestLayout = () => {
 
   return (
     <div>
-      <Grid container direction='column'>
         <Grid item>
           <Header />
         </Grid>
+        <Container maxWidth="lg">
         <h2>Products</h2>
-        <Grid item container>
-          <Grid item xs={false} sm={2} />
-          <Grid item xs={12} sm={8}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+        >
             <Grid container spacing={2}>
               {Object.keys(products).map((id) => (
-                <Grid item xs={12} md={6}><ProductCard product={products[id]} /></Grid>
+                <Grid item xs={12} sm={6} lg={4}><ProductCard product={products[id]} /></Grid>
               ))}
             </Grid>
-          </Grid>
-          <Grid item xs={false} sm={2} />
+
+          
         </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 };
